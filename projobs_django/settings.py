3ac69d176fa5464a2a94adeb7b1b80704a76fc2f
AdministrativoPj <projobsaddmin2025@gmail.com>
2025-08-01 02:14:28 -0500

    imagenes bug

--- projobs_django/settings.py
+++ projobs_django/settings.py
@@ -94,6 +94,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+DEBUG = True 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- projobs_django/urls.py
+++ projobs_django/urls.py
@@ -3,8 +3,15 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projobs.urls')),
-    path('oauth/', include('social_django.urls', namespace='social'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('oauth/', include('social_django.urls', namespace='social')),
+]
+
+# ⚠️ Este bloque debe estar AFUERA del array principal
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
